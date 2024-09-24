b4aa1b1847223b2a0617f01aa10bb6f576cbd84d
luciorim <88190686+luciorim@users.noreply.github.com>
2024-09-25 01:32:16 +0500

    initial

---
+++ src/main/java/kz/library/task/ufuture/UfutureApplication.java
@@ -0,0 +1,13 @@
+package kz.library.task.ufuture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfutureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UfutureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/library/task/ufuture/UfutureApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.library.task.ufuture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfutureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
